54b54a204448c80b7fe58df7cf76ad5a2cae6769
Adam Beguelin <adam@sensr.net>
2014-05-21 18:32:06 -0700

    Date parsing more flexible. Fix typo in date parsing error. Camera pasring more strict. Verbose actually does something.

--- get-images.rb
+++ get-images.rb
@@ -1,3 +1,4 @@
+require 'date'
 require 'optparse'
 require 'sensr'
 require 'pp'
@@ -13,14 +14,14 @@ optparse = OptionParser.new do|opts|
 # support -d all
   options[:date] = nil
   opts.on( '-d', '--date yyyy-mm-dd', 'Date grab the images from [year-month-day]' ) do |date| # TODO add optional month and day
-    if m = date.match(/^(\d{4})-(\d\d)-(\d\d)$/)
+    if m = date.match(/^(\d{4})-(\d\d?)-(\d\d?)$/)
       year = m[1]
       month = m[2]
       day = m[3]
 
       options[:date] =  Time.utc(year,month,day).to_i
     else
-      puts "Exception: Error in specified duration '#{date}'. Exiting."
+      puts "Exception: Error in specified date '#{date}'. Exiting."
       exit
     end
   end
@@ -28,7 +29,7 @@ optparse = OptionParser.new do|opts|
   
   options[:camera] = nil
   opts.on( '-c', '--cam n', 'Camera id to pull images from [n]' ) do |cam| 
-    if m = cam.match(/^(\d+)/)
+    if m = cam.match(/^(\d+)$/)
       num = m[1]
       options[:camera] =  num.to_i
     else
@@ -62,9 +63,11 @@ begin
   options[:date] = Time.now.to_i if options[:date].nil?
   date = options[:date]
   
-
+  if options[:verbose] then
+    puts "finding images for camera #{cam_id} on day #{Time.at(date).to_datetime}"
+  end
+  
   c = Sensr::Camera.find(cam_id)
-
 
   day =  c.day(date)
   day["day"]["hours"].each do |hour|
